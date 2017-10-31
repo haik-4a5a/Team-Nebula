f3bfd5751d41c1b92a1eab6fb01bb811a4ddc609
Mark <500306@edu.rocmn.nl>
2017-10-31 13:42:29 +0100

    Back button

--- StageApp/src/pages/beoordeling2/beoordeling2.ts
+++ StageApp/src/pages/beoordeling2/beoordeling2.ts
@@ -86,7 +86,7 @@ export class BeoordelingPageTwo {
     };
   }
 
-  public save(){
+  public back(){
     this.bshizzles.update(this.FormId, {
     a16: this.bform.a16,
     o16: this.bform.o16,
@@ -109,8 +109,33 @@ export class BeoordelingPageTwo {
     ah: this.bform.ah,
     oh: this.bform.oh
   })
-  this.navCtrl.pop().then(()=>{
-    this.navCtrl.pop()
-  });    
+  this.navCtrl.pop()
+}
+public save(){
+  this.bshizzles.update(this.FormId, {
+  a16: this.bform.a16,
+  o16: this.bform.o16,
+  a17: this.bform.a17,
+  o17: this.bform.o17,
+  a18: this.bform.a18,
+  o18: this.bform.o18,
+  a19: this.bform.a19,
+  o19: this.bform.o19,
+  a20: this.bform.a20,
+  o20: this.bform.o20,
+  a21: this.bform.a21,
+  o21: this.bform.o21,
+  a22: this.bform.a22,
+  o22: this.bform.o22,
+  a23: this.bform.a23,
+  o23: this.bform.o23,
+  a24: this.bform.a24,
+  o24: this.bform.o24,
+  ah: this.bform.ah,
+  oh: this.bform.oh
+})
+this.navCtrl.pop().then(()=>{
+  this.navCtrl.pop()
+});    
 }
 }
